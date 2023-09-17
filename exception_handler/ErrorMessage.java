c91d3ae9a04c27e6aee11d28d38dbeaa6c483f08
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-09-17 17:01:38 +0530

    Common Exception package in (Spring-Boot) || Copy-Paste

--- exception_handler/ErrorMessage.java
+++ exception_handler/ErrorMessage.java
@@ -9,8 +9,8 @@ public class ErrorMessage {
   private String description;
 
   public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
-    this.statusCode = statusCode;
-    this.timestamp = timestamp;
+      this.statusCode = statusCode;
+      this.timestamp = timestamp;
     this.message = message;
     this.description = description;
   }
