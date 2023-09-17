29b7eed028e04cab7ce7fe249cddf2e74db6e1b5
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-09-17 17:03:03 +0530

    Common Exception package in (Spring-Boot) || Copy-Paste

--- exception_custom/ResourceNotFoundException.java
+++ exception_custom/ResourceNotFoundException.java
@@ -2,9 +2,9 @@ package com.exception_custom;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-	  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-	  public ResourceNotFoundException(String msg) {
+	 public ResourceNotFoundException(String msg) {
 	    super(msg);
-	  }
+	}
 }
